8a91fba1a081d4bc995cffdc1a73a15e7ce9d4da
RFNshare <rfnshare@gmail.com>
2020-06-17 13:48:37 +0600

    Done with first lesson, Basic JS alret

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+// alert("Hello There");
+// prompt("Enter you unique code to access this site: ");
+console.log("Hello to Console");
+document.write("Time to learn JavaScript");
+
+function Test()
+{
+    document.getElementById("test").innerHTML = "Hello World";
+}
+
