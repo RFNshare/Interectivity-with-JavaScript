72af269f771002d3d3db2cbc7f71aef788174f0f
RFNshare <rfnshare@gmail.com>
2020-06-18 11:52:59 +0600

    Some changes and end session

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,22 +1,16 @@
-// alert("Hello There");
-// prompt("Enter you unique code to access this site: ");
-// console.log("Hello to Console");
-// document.write("Time to learn JavaScript");
-//
-// function Test()
-// {
-//     document.getElementById("test").innerHTML = "Hello World";
-// }
-
-// var name = prompt("Write your name, and see magic 🥰")
-// console.log(name);
-// // document.write("<h1>" + name + "</h1>");
-// document.write("Page URL: " + document.baseURI + "<br>");
-// document.write("Page tiles: " + document.title + "<br>");
+var name = prompt("Write your name, and see magic 🥰")
+console.log(name);
+document.write("<h1>" + name + "</h1>");
+document.write("Page URL: " + document.baseURI + "<br>");
+document.write("Page tiles: " + document.title + "<br>");
 var pars = document.getElementsByTagName('p');
 document.writeln(pars + "<br>");
 function count() {
     document.write(pars.length);
+}
+function nm() {
+    document.getElementById("test").innerHTML = name;
+
 }
 var pr = "Hello from function parameter";
 function para(pr) {
@@ -29,8 +23,11 @@ function welcomeMsg(msg) {
     var name = prompt("What is your name?");
     return name;
 }
-function nm() {
-    document.getElementById("test").innerHTML = name;
+function message(msg) {
+    document.getElementById('output').innerHTML = msg + " munataha";
 }
-var firstName = welcomeMsg("Hi");
+function  dsiplayDate() {
+    document.getElementById("date").innerHTML = Date();
+
+}
 
